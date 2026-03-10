5d763617a2badf48576058d19d13654350e2b87b
Wall <90075138+Wall-core@users.noreply.github.com>
2026-03-10 13:14:05 -0400

    Remove unused variable. (#3228)
    (cherry picked from commit d1c0292cf0b8fe700fc0a8e9a899d3339081ba64)

--- src/game/Spells/Spell.cpp
+++ src/game/Spells/Spell.cpp
@@ -8359,11 +8359,6 @@ void Spell::ResetEffectDamageAndHeal()
     m_absorbed = 0;
 }
 
-void Spell::SetClientStarted(bool bisClientStarted)
-{
-    m_isClientStarted = bisClientStarted;
-}
-
 void Spell::OnSpellLaunch()
 {
     if (!m_casterUnit || !m_casterUnit->IsInWorld())
