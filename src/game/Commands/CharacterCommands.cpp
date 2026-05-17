b235ae7a221809f14b8719eab2ed3e609df0149d
killerwife <killerwife@gmail.com>
2026-05-17 09:54:13 +0300

    Implement passing of bag slot for EQUIP_ERR_ITEM_DOESNT_GO_INTO_BAG2.
    https://github.com/cmangos/mangos-wotlk/commit/fef8d93190482aaacf42f8831ae377bf7710925f
    
    (cherry picked from commit fb245b21dab332de07734eae79af62232f49a748)

--- src/game/Commands/CharacterCommands.cpp
+++ src/game/Commands/CharacterCommands.cpp
@@ -3552,7 +3552,7 @@ bool ChatHandler::HandleAddItemSetCommand(char* args)
             }
             else
             {
-                pl->SendEquipError(msg, nullptr, nullptr, itr.second.ItemId);
+                pl->SendEquipError(msg, nullptr, nullptr, 0, itr.second.ItemId);
                 PSendSysMessage(LANG_ITEM_CANNOT_CREATE, itr.second.ItemId, 1);
             }
         }
